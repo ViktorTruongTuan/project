7db50897661139074161f56bfc3366ba50343a6f
ViktorTruongTuan <80935344+ViktorTruongTuan@users.noreply.github.com>
2021-11-08 17:18:50 +0700

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { CaroselComponent } from './carosel/carosel.component';
 import { LoginComponent } from './login/login.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ProductlistComponent } from './productlist/productlist.component';
@@ -12,7 +10,6 @@ import { CarouselComponent } from './carousel/carousel.component';
 @NgModule({
   declarations: [
     AppComponent,
-    CaroselComponent,
     LoginComponent,
     NavbarComponent,
     ProductlistComponent,
